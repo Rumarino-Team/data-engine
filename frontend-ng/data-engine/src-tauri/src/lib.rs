315502ec7c8847b4667056ec17e19bf1bb6b1462
HarenDev <supercucoboy@gmail.com>
2026-04-30 16:38:52 -0400

    Workarounds for Tauri

--- frontend-ng/data-engine/src-tauri/src/lib.rs
+++ frontend-ng/data-engine/src-tauri/src/lib.rs
@@ -1,5 +1,10 @@
+use std::env;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    // Fix WebKit crashes on Wayland
+    env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+    
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .run(tauri::generate_context!())
